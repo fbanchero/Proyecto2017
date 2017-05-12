b18b3812f54a38ae2417357f2796e21b191ce1f3
fbanchero <rfbanchero@gmail.com>
2017-05-11 23:28:33 -0300

    merge

--- backend/api/src/api/helpers/ElementBuilder.java
+++ backend/api/src/api/helpers/ElementBuilder.java
@@ -38,13 +38,14 @@ import ifml.extensions.SelectEvent;
 import ifml.extensions.SimpleField;
 import ifml.extensions.SubmitEvent;
 import ifml.extensions.TextField;
+import ifml.extensions.Video;
 import ifml.extensions.Window;
 
 public class ElementBuilder {
-
-	public static CoreFactory f;
-	public static ExtensionsFactory ef;
-	public static UMLFactory umlf;
+	
+    public static CoreFactory f;
+    public static ExtensionsFactory ef;
+    public static UMLFactory umlf;
 	public static TypeFactory tf;
 	public static EcoreFactory ecf;
 
@@ -62,308 +63,368 @@ public class ElementBuilder {
 	/**
 	 * Creates a Window based on a MockupElement.
 	 * 
-	 * @param elem
-	 *            - MockupElement with all the relevant info.
-	 * @return - Window object.
+	 * @param elem - MockupElement with all the relevant info.
+	 * @return     - Window object.
 	 */
 	public Window createWindow(MockupSingleColumnElement elem) {
-
+		
 		Window window = ef.createWindow();
 		window.setId(elem.getId());
 		window.setName(elem.getName());
 		window.setIsModal(true);
-
+		
 		return window;
-
+		
 	}
 
 	/**
 	 * Creates a List based on a MockupElement.
 	 * 
-	 * @param elem
-	 *            - MockupElement with all the relevant info.
-	 * @return - List object.
+	 * @param elem - MockupElement with all the relevant info.
+	 * @return     - List object.
 	 */
-	public List createList(MockupMultipleColumnElement elem, HashMap<String, NavigationFlow> links) {
-
+	public List createList(MockupMultipleColumnElement elem, ArrayList<LinkElem> links) {
+		
 		List list = ef.createList();
 		list.setId(elem.getId());
 		list.setName(elem.getName());
-
-		if (elem.getEvents() != null && !elem.getEvents().get(0).getLink().equals("")) {
+		
+		if(elem.getEvents() != null && !elem.getEvents().get(0).getLink().equals("")) {
 			SelectEvent se = createSelectEvent(elem);
 			NavigationEvent ne = elem.getEvents().get(0);
 			NavigationFlow nf = f.createNavigationFlow();
 
 			nf.setSrcInteractionFlowElement(se);
-			se.getNavigationFlows().add(nf);
+			se.getNavigationFlows().add(nf);	
 			list.getViewElementEvents().add(se);
-			links.put(ne.getLink(), nf);
+			LinkElem le = new LinkElem();
+			le.setId(ne.getLink());
+			le.setNavigationFlow(nf);
+			
+			links.add(le);
 		}
-
+		
 		return list;
-
+		
 	}
 
 	/**
 	 * Creates a Button based on a MockupElement.
 	 * 
-	 * @param elem
-	 *            - MockupElement with all the relevant info.
-	 * @return - Button object.
+	 * @param elem - MockupElement with all the relevant info.
+	 * @return     - Button object.
 	 */
-	public Button createButton(MockupMultipleColumnElement elem, HashMap<String, NavigationFlow> links) {
-
+	public Button createButton(MockupMultipleColumnElement elem, ArrayList<LinkElem> links) {
+		
 		Button button = ef.createButton();
 		button.setId(elem.getId());
 		button.setName(elem.getName());
 		HashMap<String, Object> properties = elem.getProperties();
-
+		
 		if (properties.containsKey("align")) {
-			button.setAlign((String) properties.get("align"));
+			button.setAlign((String) properties.get("align")); 
 		}
-
+		
 		if (properties.containsKey("value")) {
-			button.setButtonText((String) elem.getProperties().get("value"));
+			button.setButtonText((String)elem.getProperties().get("value"));
 		}
 
-		if (elem.getEvents() != null) {
-			for (NavigationEvent navEvent : elem.getEvents()) {
-
+		if(elem.getEvents() != null){
+			for(NavigationEvent navEvent: elem.getEvents()){
+				
 				ViewElementEvent event = f.createViewElementEvent();
 				NavigationFlow nf = f.createNavigationFlow();
-
+				
 				nf.setSrcInteractionFlowElement(event);
 				event.getNavigationFlows().add(nf);
 				button.getViewElementEvents().add(event);
-
-				links.put(navEvent.getLink(), nf);
-
+				LinkElem le = new LinkElem();
+				le.setId(navEvent.getLink());
+				le.setNavigationFlow(nf);
+				
+				links.add(le);
+				
 			}
 		}
-
+	    
 		return button;
-
+		
 	}
 
 	/**
 	 * Creates a TextField based on a MockupElement.
 	 * 
-	 * @param elem
-	 *            - MockupElement with all the relevant info.
-	 * @return - TextField object.
+	 * @param elem - MockupElement with all the relevant info.
+	 * @return     - TextField object.
 	 */
-	public TextField createTextField(MockupMultipleColumnElement elem, HashMap<String, NavigationFlow> links) {
-
+	public TextField createTextField(MockupMultipleColumnElement elem, ArrayList<LinkElem> links) {
+		
 		TextField textField = ef.createTextField();
 		textField.setId(elem.getId());
 		textField.setName(elem.getName());
 		HashMap<String, Object> properties = elem.getProperties();
-
+		
 		if (properties.containsKey("fontSize")) {
-			textField.setFontSize((String) properties.get("fontSize"));
+			textField.setFontSize((String) properties.get("fontSize")); 
 		}
-
+		
 		if (properties.containsKey("value")) {
-			textField.setTextFieldText((String) elem.getProperties().get("value"));
+			textField.setTextFieldText((String)elem.getProperties().get("value"));
 		}
-
-		if (elem.getEvents() != null) {
-			for (NavigationEvent navEvent : elem.getEvents()) {
-
+		
+		if(elem.getEvents() != null){
+			for(NavigationEvent navEvent: elem.getEvents()){
+				
 				ViewElementEvent event = f.createViewElementEvent();
 				NavigationFlow nf = f.createNavigationFlow();
-
+				
 				nf.setSrcInteractionFlowElement(event);
 				event.getNavigationFlows().add(nf);
 				textField.getViewElementEvents().add(event);
-
-				links.put(navEvent.getLink(), nf);
-
+				LinkElem le = new LinkElem();
+				le.setId(navEvent.getLink());
+				le.setNavigationFlow(nf);
+				
+				links.add(le);
+				
 			}
 		}
-
+	    
 		return textField;
-
+		
 	}
-
+	
 	/**
 	 * Creates a ViewContainer based on a MockupElement.
 	 * 
-	 * @param elem
-	 *            - MockupElement with all the relevant info.
-	 * @return - ViewContainer object.
+	 * @param elem - MockupElement with all the relevant info.
+	 * @return     - ViewContainer object.
 	 */
-	public ViewContainer createViewContainer(MockupGeneralElement elem, HashMap<String, NavigationFlow> links) {
-
+	public ViewContainer createViewContainer(MockupGeneralElement elem, ArrayList<LinkElem> links) {
+		
 		ViewContainer vc = f.createViewContainer();
 		vc.setId(elem.getId());
 		vc.setName(elem.getName());
 		vc.setIsXor(elem.getType().equals("tabs"));
-
-		if (elem.getEvents() != null) {
-
-			for (NavigationEvent navEvent : elem.getEvents()) {
+		
+		if (elem.getEvents() != null){
+			
+			for(NavigationEvent navEvent: elem.getEvents()){
 				ViewElementEvent event = f.createViewElementEvent();
 				NavigationFlow nf = f.createNavigationFlow();
-
+				
 				nf.setSrcInteractionFlowElement(event);
 				event.getNavigationFlows().add(nf);
 				vc.getViewElementEvents().add(event);
-
-				links.put(navEvent.getLink(), nf);
-
+				LinkElem le = new LinkElem();
+				le.setId(navEvent.getLink());
+				le.setNavigationFlow(nf);
+				
+				links.add(le);
+				
 			}
-
+		
 		}
-
+		
 		return vc;
-
+		
 	}
-
+	
 	/**
 	 * Creates a ViewElement based on a MockupElement.
 	 * 
-	 * @param elem
-	 *            - MockupElement with all the relevant info.
-	 * @return - ViewElement object.
+	 * @param elem - MockupElement with all the relevant info.
+	 * @return     - ViewElement object.
 	 */
-	public ViewElement createViewElement(MockupGeneralElement elem, HashMap<String, NavigationFlow> links) {
-
+	public ViewElement createViewElement(MockupGeneralElement elem, ArrayList<LinkElem> links) {
+		
 		ViewElement ve = f.createViewElement();
 		ve.setId(elem.getId());
 		ve.setName(elem.getName());
-
+		
 		if (elem.getEvents() != null) {
-			for (NavigationEvent navEvent : elem.getEvents()) {
-
+			for (NavigationEvent navEvent: elem.getEvents()) {
+				
 				ViewElementEvent event = f.createViewElementEvent();
 				NavigationFlow nf = f.createNavigationFlow();
-
+				
 				nf.setSrcInteractionFlowElement(event);
 				event.getNavigationFlows().add(nf);
 				ve.getViewElementEvents().add(event);
-
-				links.put(navEvent.getLink(), nf);
-
+				LinkElem le = new LinkElem();
+				le.setId(navEvent.getLink());
+				le.setNavigationFlow(nf);
+				
+				links.add(le);
+				
 			}
 		}
-
+		
 		return ve;
-
+		
 	}
-
+	
 	/**
 	 * Creates a SubmitEvent based on a MockupElement.
 	 * 
-	 * @param elem
-	 *            - MockupElement with all the relevant info.
-	 * @return - SubmitEvent object.
+	 * @param elem - MockupElement with all the relevant info.
+	 * @return     - SubmitEvent object.
 	 */
 	public SubmitEvent createSubmitEvent(MockupGeneralElement elem) {
-
+		
 		SubmitEvent ve = ef.createSubmitEvent();
 		ve.setId(elem.getId());
 		ve.setName(elem.getName());
 		// TODO: add remaining properties.
-
+		
 		return ve;
-
+		
 	}
 
 	/**
 	 * Creates a SelectEvent based on a MockupElement.
 	 * 
-	 * @param elem
-	 *            - MockupElement with all the relevant info.
-	 * @return - SelectEvent object.
+	 * @param elem - MockupElement with all the relevant info.
+	 * @return     - SelectEvent object.
 	 */
 	public static SelectEvent createSelectEvent(MockupGeneralElement elem) {
-
+		
 		SelectEvent se = ef.createSelectEvent();
 		se.setId(elem.getId());
 		se.setName(elem.getName());
 
 		return se;
-
+		
 	}
-
+	
 	/**
 	 * Creates a SimpleField based on a MockupElement.
 	 * 
-	 * @param elem
-	 *            - MockupElement with all the relevant info.
-	 * @return - SimpleField object.
+	 * @param elem - MockupElement with all the relevant info.
+	 * @return     - SimpleField object.
 	 */
 	public SimpleField createSimpleField(MockupGeneralElement elem) {
-
+		
 		SimpleField sf = ef.createSimpleField();
 		sf.setId(elem.getId());
 		sf.setName(elem.getName());
-
+		
 		// TODO: add remaining properties.
 		HashMap<String, Object> properties = elem.getProperties();
-
+		
 		if (properties.containsKey("placeholder")) {
-			// sf.setPlaceholder((String) properties.get("placeholder"));
+			// sf.setPlaceholder((String) properties.get("placeholder")); 
 		}
-
+		
 		if (properties.containsKey("label")) {
 			// sf.setLabel((String)elem.getProperties().get("label"));
 		}
-
+		
 		return sf;
-
+		
 	}
 
 	/**
 	 * Creates a Image based on a MockupElement.
 	 * 
-	 * @param elem
-	 *            - MockupElement with all the relevant info.
-	 * @return - Image object.
+	 * @param elem - MockupElement with all the relevant info.
+	 * @return     - Image object.
 	 */
-	public Image createImage(MockupGeneralElement elem, HashMap<String, NavigationFlow> links) {
-
+	public Image createImage(MockupGeneralElement elem, ArrayList<LinkElem> links) {
+		
 		Image img = ef.createImage();
 		img.setId(elem.getId());
 		img.setName(elem.getName());
 		HashMap<String, Object> properties = elem.getProperties();
-
+		
 		if (properties.containsKey("align")) {
-			img.setAlign((String) properties.get("align"));
+			img.setAlign((String) properties.get("align")); 
 		}
-
+		
 		if (properties.containsKey("url")) {
-			img.setUrl((String) elem.getProperties().get("url"));
+			img.setUrl((String) properties.get("url"));
 		}
-
+		
+		if (properties.containsKey("width")) {
+			img.setWidth(Double.parseDouble((String) elem.getProperties().get("width"))); 
+		}
+		
 		if (elem.getEvents() != null) {
-			for (NavigationEvent navEvent : elem.getEvents()) {
-
+			for (NavigationEvent navEvent: elem.getEvents()) {
+				
 				ViewElementEvent event = f.createViewElementEvent();
 				NavigationFlow nf = f.createNavigationFlow();
-
+				
 				nf.setSrcInteractionFlowElement(event);
 				event.getNavigationFlows().add(nf);
 				img.getViewElementEvents().add(event);
-
-				links.put(navEvent.getLink(), nf);
-
+				LinkElem le = new LinkElem();
+				le.setId(navEvent.getLink());
+				le.setNavigationFlow(nf);
+				
+				links.add(le);
+				
 			}
-		}
-
+		}	
+		
 		return img;
-
+		
 	}
-
 	/**
-	 * Creates a Image based on a MockupElement.
+	 * Creates a Video based on a MockupElement.
 	 * 
-	 * @param elem
-	 *            - MockupElement with all the relevant info.
-	 * @return - Image object.
+	 * @param elem - MockupElement with all the relevant info.
+	 * @return     - Video object.
 	 */
+	public Video createVideo(MockupGeneralElement elem, ArrayList<LinkElem> links) {
+		
+		Video vid = ef.createVideo();
+		vid.setId(elem.getId());
+		vid.setName(elem.getName());
+		HashMap<String, Object> properties = elem.getProperties();
+		
+		if (properties.containsKey("align")) {
+			vid.setAlign((String) properties.get("align")); 
+		}
+		
+		if (properties.containsKey("url")) {
+			String url = (String)elem.getProperties().get("url");
+			if(url.contains("watch?v=")){
+				url = url.replace("watch?v=", "embed/");
+			}
+			vid.setUrl(url);
+		}
+		
+		if (properties.containsKey("height")) {
+			vid.setHeight(Double.parseDouble((String) elem.getProperties().get("height")));
+		}
+		
+		if (properties.containsKey("width")) {
+			vid.setWidth(Double.parseDouble((String) elem.getProperties().get("width")));
+		}
+		
+		if (elem.getEvents() != null) {
+			for (NavigationEvent navEvent: elem.getEvents()) {
+				
+				ViewElementEvent event = f.createViewElementEvent();
+				NavigationFlow nf = f.createNavigationFlow();
+				
+				nf.setSrcInteractionFlowElement(event);
+				event.getNavigationFlows().add(nf);
+				vid.getViewElementEvents().add(event);
+				LinkElem le = new LinkElem();
+				le.setId(navEvent.getLink());
+				le.setNavigationFlow(nf);
+				
+				links.add(le);
+				
+			}
+		}	
+		
+		return vid;
+		
+	}
+	
 //	public DomainConcept createDomainConcept(DomainClass domainClass) {
 //
 //		UMLDomainConcept dc = f.createUMLDomainConcept();
@@ -434,5 +495,5 @@ public class ElementBuilder {
 		}
 		return a;
 	}
-
+	
 }
