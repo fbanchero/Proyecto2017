8a01c671fbc6b26d3ff23ef87279ae20b0ce3efa
fbanchero <rfbanchero@gmail.com>
2017-05-11 22:18:12 -0300

    Merge

---
+++ ifml/src/ifml/extensions/Video.java
@@ -0,0 +1,131 @@
+/**
+ */
+package ifml.extensions;
+
+import ifml.core.ViewElement;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Video</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * </p>
+ * <ul>
+ *   <li>{@link ifml.extensions.Video#getHeight <em>Height</em>}</li>
+ *   <li>{@link ifml.extensions.Video#getAlign <em>Align</em>}</li>
+ *   <li>{@link ifml.extensions.Video#getUrl <em>Url</em>}</li>
+ *   <li>{@link ifml.extensions.Video#getWidth <em>Width</em>}</li>
+ * </ul>
+ *
+ * @see ifml.extensions.ExtensionsPackage#getVideo()
+ * @model
+ * @generated
+ */
+public interface Video extends ViewElement {
+	/**
+	 * Returns the value of the '<em><b>Height</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Height</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Height</em>' attribute.
+	 * @see #setHeight(double)
+	 * @see ifml.extensions.ExtensionsPackage#getVideo_Height()
+	 * @model
+	 * @generated
+	 */
+	double getHeight();
+
+	/**
+	 * Sets the value of the '{@link ifml.extensions.Video#getHeight <em>Height</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Height</em>' attribute.
+	 * @see #getHeight()
+	 * @generated
+	 */
+	void setHeight(double value);
+
+	/**
+	 * Returns the value of the '<em><b>Align</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Align</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Align</em>' attribute.
+	 * @see #setAlign(String)
+	 * @see ifml.extensions.ExtensionsPackage#getVideo_Align()
+	 * @model
+	 * @generated
+	 */
+	String getAlign();
+
+	/**
+	 * Sets the value of the '{@link ifml.extensions.Video#getAlign <em>Align</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Align</em>' attribute.
+	 * @see #getAlign()
+	 * @generated
+	 */
+	void setAlign(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Url</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Url</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Url</em>' attribute.
+	 * @see #setUrl(String)
+	 * @see ifml.extensions.ExtensionsPackage#getVideo_Url()
+	 * @model
+	 * @generated
+	 */
+	String getUrl();
+
+	/**
+	 * Sets the value of the '{@link ifml.extensions.Video#getUrl <em>Url</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Url</em>' attribute.
+	 * @see #getUrl()
+	 * @generated
+	 */
+	void setUrl(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Width</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Width</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Width</em>' attribute.
+	 * @see #setWidth(double)
+	 * @see ifml.extensions.ExtensionsPackage#getVideo_Width()
+	 * @model
+	 * @generated
+	 */
+	double getWidth();
+
+	/**
+	 * Sets the value of the '{@link ifml.extensions.Video#getWidth <em>Width</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Width</em>' attribute.
+	 * @see #getWidth()
+	 * @generated
+	 */
+	void setWidth(double value);
+
+} // Video
---
+++ ifml/src/ifml/extensions/impl/VideoImpl.java
@@ -0,0 +1,326 @@
+/**
+ */
+package ifml.extensions.impl;
+
+import ifml.core.impl.ViewElementImpl;
+
+import ifml.extensions.ExtensionsPackage;
+import ifml.extensions.Video;
+
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Video</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * </p>
+ * <ul>
+ *   <li>{@link ifml.extensions.impl.VideoImpl#getHeight <em>Height</em>}</li>
+ *   <li>{@link ifml.extensions.impl.VideoImpl#getAlign <em>Align</em>}</li>
+ *   <li>{@link ifml.extensions.impl.VideoImpl#getUrl <em>Url</em>}</li>
+ *   <li>{@link ifml.extensions.impl.VideoImpl#getWidth <em>Width</em>}</li>
+ * </ul>
+ *
+ * @generated
+ */
+public class VideoImpl extends ViewElementImpl implements Video {
+	/**
+	 * The default value of the '{@link #getHeight() <em>Height</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getHeight()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final double HEIGHT_EDEFAULT = 0.0;
+
+	/**
+	 * The cached value of the '{@link #getHeight() <em>Height</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getHeight()
+	 * @generated
+	 * @ordered
+	 */
+	protected double height = HEIGHT_EDEFAULT;
+
+	/**
+	 * The default value of the '{@link #getAlign() <em>Align</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getAlign()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String ALIGN_EDEFAULT = null;
+
+	/**
+	 * The cached value of the '{@link #getAlign() <em>Align</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getAlign()
+	 * @generated
+	 * @ordered
+	 */
+	protected String align = ALIGN_EDEFAULT;
+
+	/**
+	 * The default value of the '{@link #getUrl() <em>Url</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getUrl()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String URL_EDEFAULT = null;
+
+	/**
+	 * The cached value of the '{@link #getUrl() <em>Url</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getUrl()
+	 * @generated
+	 * @ordered
+	 */
+	protected String url = URL_EDEFAULT;
+
+	/**
+	 * The default value of the '{@link #getWidth() <em>Width</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getWidth()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final double WIDTH_EDEFAULT = 0.0;
+
+	/**
+	 * The cached value of the '{@link #getWidth() <em>Width</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getWidth()
+	 * @generated
+	 * @ordered
+	 */
+	protected double width = WIDTH_EDEFAULT;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected VideoImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return ExtensionsPackage.Literals.VIDEO;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public double getHeight() {
+		return height;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setHeight(double newHeight) {
+		double oldHeight = height;
+		height = newHeight;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, ExtensionsPackage.VIDEO__HEIGHT, oldHeight, height));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getAlign() {
+		return align;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setAlign(String newAlign) {
+		String oldAlign = align;
+		align = newAlign;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, ExtensionsPackage.VIDEO__ALIGN, oldAlign, align));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getUrl() {
+		return url;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setUrl(String newUrl) {
+		String oldUrl = url;
+		url = newUrl;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, ExtensionsPackage.VIDEO__URL, oldUrl, url));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public double getWidth() {
+		return width;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setWidth(double newWidth) {
+		double oldWidth = width;
+		width = newWidth;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, ExtensionsPackage.VIDEO__WIDTH, oldWidth, width));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case ExtensionsPackage.VIDEO__HEIGHT:
+				return getHeight();
+			case ExtensionsPackage.VIDEO__ALIGN:
+				return getAlign();
+			case ExtensionsPackage.VIDEO__URL:
+				return getUrl();
+			case ExtensionsPackage.VIDEO__WIDTH:
+				return getWidth();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case ExtensionsPackage.VIDEO__HEIGHT:
+				setHeight((Double)newValue);
+				return;
+			case ExtensionsPackage.VIDEO__ALIGN:
+				setAlign((String)newValue);
+				return;
+			case ExtensionsPackage.VIDEO__URL:
+				setUrl((String)newValue);
+				return;
+			case ExtensionsPackage.VIDEO__WIDTH:
+				setWidth((Double)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case ExtensionsPackage.VIDEO__HEIGHT:
+				setHeight(HEIGHT_EDEFAULT);
+				return;
+			case ExtensionsPackage.VIDEO__ALIGN:
+				setAlign(ALIGN_EDEFAULT);
+				return;
+			case ExtensionsPackage.VIDEO__URL:
+				setUrl(URL_EDEFAULT);
+				return;
+			case ExtensionsPackage.VIDEO__WIDTH:
+				setWidth(WIDTH_EDEFAULT);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case ExtensionsPackage.VIDEO__HEIGHT:
+				return height != HEIGHT_EDEFAULT;
+			case ExtensionsPackage.VIDEO__ALIGN:
+				return ALIGN_EDEFAULT == null ? align != null : !ALIGN_EDEFAULT.equals(align);
+			case ExtensionsPackage.VIDEO__URL:
+				return URL_EDEFAULT == null ? url != null : !URL_EDEFAULT.equals(url);
+			case ExtensionsPackage.VIDEO__WIDTH:
+				return width != WIDTH_EDEFAULT;
+		}
+		return super.eIsSet(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String toString() {
+		if (eIsProxy()) return super.toString();
+
+		StringBuffer result = new StringBuffer(super.toString());
+		result.append(" (Height: ");
+		result.append(height);
+		result.append(", Align: ");
+		result.append(align);
+		result.append(", Url: ");
+		result.append(url);
+		result.append(", Width: ");
+		result.append(width);
+		result.append(')');
+		return result.toString();
+	}
+
+} //VideoImpl
